f7f5b3e89b60a21d03159b1c32fadc8510c6802c
shutthefupmeh <dafancy11@gmail.com>
2025-10-16 02:36:41 +0700

    Normalize all line endings to LF

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import useMacbookStore from "../store/index.js";
 import clsx from "clsx";
+import { Box, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import MacbookModel14 from "./models/Macbook-14.jsx";
+import StudioLights from "./models/StudioLights.jsx";
+
 
 const ProductViewer = () => {
   const { color, scale, setColor, setScale } = useMacbookStore();
@@ -9,11 +14,12 @@ const ProductViewer = () => {
     <section id="product-viewer">
       <h2> Take a closer look.</h2>
 
+      {/* color selection */}
       <div className="controls">
-        <p className="info">MacbookPro 16" in Silver / Space Black</p>
+        <p className="info">MacbookPro 16" in {color}</p>
 
         <div className="flex-center gap-5 mt-5">
-          <div className="color-contorl">
+          <div className="color-control">
             <div
               onClick={() => setColor("#adb5bd")}
               className={clsx(
@@ -21,21 +27,51 @@ const ProductViewer = () => {
                 color === "#adb5bd" && "active"
               )}
             />
-            <div className="bg-neutral-900" />
+            <div
+              onClick={() => setColor("#2e2c2e")}
+              className={clsx(
+                "bg-neutral-900",
+                color === "#2e2c2e" && "active"
+              )}
+            />
           </div>
 
+          {/* size selection */}
           <div className="size-control">
-            <div>
+            <div
+              onClick={() => setScale(0.06)}
+              className={clsx(
+                scale === 0.06
+                  ? "bg-white text-black"
+                  : "bg-transparent text-white"
+              )}
+            >
               <p>14"</p>
             </div>
-            <div>
+
+            <div
+              onClick={() => setScale(0.08)}
+              className={clsx(
+                scale === 0.08
+                  ? "bg-white text-black"
+                  : "bg-transparent text-white"
+              )}
+            >
               <p>16"</p>
             </div>
           </div>
         </div>
       </div>
 
-      <p className="text-white text-4xl">Render Canvas</p>
+      <Canvas
+        id="canvas"
+        camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}
+      >
+        <StudioLights />
+        <MacbookModel14 scale={0.06} position={[-1 , 0, 0]} />
+        <OrbitControls enableZoom={false} />
+
+      </Canvas>
     </section>
   );
 };
